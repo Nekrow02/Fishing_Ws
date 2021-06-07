69e1fbbba65491c38e074eb12275ffbcdb217be4
jckim1102 <jckim1102@naver.com>
2021-06-07 23:02:21 +0900

    Revert

--- Fishing/Fising/Assets/Scripts/Shop/Global.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Global
-{
-    public enum PlayType { START = 0, CONTINUE = 1 };
-    public static int date = 0;
-}
